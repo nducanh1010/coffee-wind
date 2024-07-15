aa724abbdec738317b8a1fe896055bc5fc144df0
duc anh <nducanh1010@gmail.com>
2024-07-16 01:10:21 +0700

    85a6cb7

---
+++ assets/D8oRLHyz.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./BlLgyE_q.js";import{A as t}from"./B85phDAE.js";import{o as e,c as r,b as a,u as c}from"./BWBFLHyk.js";import"./CAY4bpvO.js";const _={class:"w-full"},d={__name:"list",setup(l){return(m,n)=>{const s=o;return e(),r("div",_,[a(s,{"mug-list":c(t)},null,8,["mug-list"])])}}};export{d as default};
